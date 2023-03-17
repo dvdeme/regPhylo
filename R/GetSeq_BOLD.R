23eb6be0ff5e9e6deb1ff17cb53561aa211e2ac8
dvdeme <david.eme3@gmail.com>
2023-03-17 16:01:44 +0100

    Add the bold.id as on option to allow the function to request data from BOLD based on BOLD taxonomic id of the taxa

--- R/GetSeq_BOLD.R
+++ R/GetSeq_BOLD.R
@@ -5,16 +5,20 @@
 #' Life Database (BOLD) for all the species in the species list
 #' @param splist a two column table. The first column contains all the binomial species names including
 #' synonyms and the second column contains only the names that will be reported
-#' in the output table as 'species_name'.
+#' in the output table as 'species_name'. If bold.id = TRUE then the first column can contain the bold taxonomic id, 
+#' and the request will be done with the taxid.
 #' @param filename name of the output table. The format of the output follows the
 #' format provided by the bold R package.
+#' @param bold.id  if TRUE then the first column of the splist table contains the bold taxid of the taxa 
+#' (should be character), by default FALSE
+#' 
 #'
 #' @return The function returns three tables: a first table exported within the working directory
 #' with the raw output from bold package plus an additional column with the date of extraction
 #' (this table only reports metadata when the DNA sequence is present), a
 #' list of two tables that are exported into the R environment, including a summary table reporting
 #' the number of occurrences retrieved for each species, and a table reporting all
-#' the data retrieved from bold (including metadata for a recorded specimen eventhough no DNA sequence
+#' the data retrieved from bold (including metadata for a recorded specimen even though no DNA sequence
 #' is attached, as such the data present in this table and the table exported into the working directory
 #' might differ).
 
@@ -35,7 +39,7 @@
 
 #' @export GetSeq_BOLD
 
-GetSeq_BOLD = function(splist = NULL, filename = NULL) {
+GetSeq_BOLD = function(splist = NULL, filename = NULL, bold.id = FALSE) {
     NameTab = c("species_name", "processid", "sampleid", "recordID", "catalognum",
         "fieldnum", "institution_storing", "collection_code", "bin_uri", "phylum_taxID",
         "phylum_name", "class_taxID", "class_name", "order_taxID", "order_name",
@@ -61,7 +65,11 @@ GetSeq_BOLD = function(splist = NULL, filename = NULL) {
     cat(t(as.matrix(NameTab)), sep = "\t", "\n", file = filename)
     i = 1
     for (i in 1:dim(splist)[1]) {
+      if(bold.id == TRUE){
+        tab = bold::bold_seqspec(ids = as.character(splist[i, 1]))
+      } else {
         tab = bold::bold_seqspec(taxon = as.character(splist[i, 1]))
+      }
         if (is.null(dim(tab)[1])) {
             TabSum = rbind(TabSum, c(as.character(splist[i, 1]), 0))
         } else {
